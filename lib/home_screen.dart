cb8e17248992b0a63ff321e237909968a89a8b8a
unknown <paulo.henriqq8@gmail.com>
2025-09-19 15:51:34 -0400

    Add home_screen widget

---
+++ lib/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        spacing: 30,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/quiz-images/quiz-logo.png',
+            width: 200,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text(
+              'Start Quiz',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
