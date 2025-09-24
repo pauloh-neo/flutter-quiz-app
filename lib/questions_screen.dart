fdeae925eb6e1f9a66450c7d0de327539e96ed49
unknown <paulo.henriqq8@gmail.com>
2025-09-23 21:49:14 -0400

    Fix Items alignment

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -19,24 +19,31 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     return SizedBox(
       // Takes full screen's width
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
 
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTap: () {},
-            );
-          }),
-        ],
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
